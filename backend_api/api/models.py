5e7c52a9e2da868cf30a720eeb0f7808829170f2
Henry <henry.phz@yahoo.com>
2022-04-26 22:43:56 +0200

    Migrated DiaryEntry model to DataBase

--- backend_api/api/models.py
+++ backend_api/api/models.py
@@ -4,5 +4,9 @@ from django.db import models
 # Create your models here.
 class DiaryEntry(models.Model):
     pub_date = models.DateTimeField("date published")
+    title = models.CharField(max_length=20, default="No Title")
     description = models.CharField(max_length=1000)
     happyness_value = models.IntegerField(default=3)
+
+    def __str__(self):
+        return self.title
